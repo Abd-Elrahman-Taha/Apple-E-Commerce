0c0ed19afedb6950590686bc106aa777fd223b91
Abd-El Rahman Taha <b0ody66taha@gmail.com>
2026-05-07 18:34:27 +0300

    fix http

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL || 'https://bhecommerce.runasp.net/api',
+  baseURL: import.meta.env.VITE_API_BASE_URL || 'http://bhecommerce.runasp.net/api',
   headers: {
     'Content-Type': 'application/json',
   },
